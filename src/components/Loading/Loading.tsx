c3b07996711e4991cfcb152189b2806475a3ad74
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-08-21 01:29:46 -0300

    feat: add Loading component

---
+++ src/components/Loading/Loading.tsx
@@ -0,0 +1,13 @@
+import { BiLoaderAlt } from 'react-icons/bi';
+
+
+
+export function Loading() {
+  return ( 
+    <div className='pt-44 border-blue-500 border-2 h-screen'>
+      <BiLoaderAlt className="animate-spin text-[2rem] w-full"/>
+    </div>
+    
+);
+}
+
