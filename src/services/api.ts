4de82640a64bd85ce5e1aed4b3fcbd87fdcbaffd
Bertoldo Klinger <bertoldokbrj@gmail.com>
2023-08-21 01:27:23 -0300

    refactor: changed return to results

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,13 +1,9 @@
 
+export const fetchProducts = async (query: string) => {
+  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${query}`);
+  const data = await response.json();
+
+  return data.results;
+};
 
 
-export const fetchProducts = async (product: string) => {
-  const response = await fetch(`https://api.mercadolibre.com/sites/MLB/search?q=${product}`)
-
-  const data = await response.json()
-  console.log(data)
-  return data
-
-
-}
-fetchProducts('iphone')
